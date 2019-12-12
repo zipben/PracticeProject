abadbdc584b5a5b0ad8fdf19fbfe47f1d4ed7408
emmanuelthegrey <whoisemmanuel@gmail.com>
2019-12-12 10:56:54 -0500

    initial ef db create

---
+++ Practice Project/Migrations/20191212155607_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PracticeProject.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MelangePurchase",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    MerchantName = table.Column<string>(nullable: true),
+                    PurchaseDate = table.Column<DateTime>(nullable: false),
+                    Kilos = table.Column<int>(nullable: false),
+                    Price = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MelangePurchase", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MelangePurchase");
+        }
+    }
+}

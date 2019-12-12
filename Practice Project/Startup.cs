12cc29b998a7d6afaeefd1ab9dc31fc502ba88b6
emmanuelthegrey <whoisemmanuel@gmail.com>
2019-12-12 11:01:01 -0500

    update startup to support sev env using sqllite and sqlserver for anything else

--- Practice Project/Startup.cs
+++ Practice Project/Startup.cs
@@ -15,19 +15,27 @@ namespace Practice_Project
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+			Environment = env;
         }
 
         public IConfiguration Configuration { get; }
+		public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
-			services.AddDbContext<MelangePurchaseContext>(options => 
+			if(Environment.IsDevelopment()){
+					services.AddDbContext<MelangePurchaseContext>(options => 
 			options.UseSqlite(Configuration.GetConnectionString("MelangePurchaseContext")));
+			}else{
+					services.AddDbContext<MelangePurchaseContext>(options => 
+			options.UseSqlServer(Configuration.GetConnectionString("MelangePurchaseContext")));
+			}
+            services.AddControllersWithViews();
+		
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
